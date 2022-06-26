e9abd32aba347ea38fd01a162b1f5edfe18483d7
NILAY SARDAR <nilay.sardar04121998@gmail.com>
2022-06-26 13:13:39 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+//find ascii value of any characters
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character here to get ascii value of that character: ");
+    scanf("%c", &ch);
+    printf("\n");
+    printf("ASCII value of [%c] is %d", ch ,ch);
+    return 0;
+}
